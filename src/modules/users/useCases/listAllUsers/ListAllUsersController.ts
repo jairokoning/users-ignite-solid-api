9f1f98ec708fdafa44aae1d861b7278fbda924ef
Jairo Koning <jairo.koning@gmail.com>
2021-04-01 05:57:44 -0300

    refact: complete challenge

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -2,11 +2,23 @@ import { Request, Response } from "express";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
+interface IRequest {
+  user_id: string;
+}
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers;
+      const all = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
+
+      return response.json(all);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
